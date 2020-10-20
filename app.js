6856b54a1e0b75e7305f84cee523bec0e3888226
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-10-20 14:45:12 -0700

    [app.js] solution for sort exercise and reduce

--- app.js
+++ app.js
@@ -59,18 +59,35 @@ const oldest = inventors.sort(function (a, b) {
 
 console.table(oldest)
 
-      // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
-      // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
-      const category = document.querySelector('.mw-category');
-      const links = Array.from(category.querySelectorAll('a'));
-      const de = links
-                  .map(link => link.textContent)
-                  .filter(streetName => streetName.includes('de'));
+// const category = document.querySelector('.mw-category');
+// const links = Array.from(category.querySelectorAll('a'));
+// const de = links
+//   .map(link => link.textContent)
+//   .filter(streetName => streetName.includes('de'));
 
-      // 7. sort Exercise
-      // Sort the people alphabetically by last name
-
+// 7. sort Exercise
+// Sort the people alphabetically by last name
+const alpha = people.sort((lastOne, nextOne) => {
+  const [aLast, aFirst] = lastOne.split(', ')
+  const [bLast, bFirst] = nextOne.split(', ')
+  return aLast > bLast ? 1 : -1;
+})
+console.table(alpha)
 
       // 8. Reduce Exercise
       // Sum up the instances of each of these
+
+const data = ['car', 'car', 'car', 'truck', 'truck', 'walk', 'walk', 'bike', 'bike', 'truck', 'van', 'run', 'van']
+
+const transportation = data.reduce(function(obj, item) {
+  if (!obj[item]) {
+    obj[item] = 0
+  } 
+  obj[item] ++
+  return obj
+}, {})
+
+console.log(transportation)
